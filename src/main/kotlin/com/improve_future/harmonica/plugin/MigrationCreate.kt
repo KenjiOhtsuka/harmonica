3697a77ae80ae0c15948ace23f00932f9655b466
Kenji Otsuka <kenji.otsuka@eys-style.com>
2018-05-12 21:24:49 +0900

    update task

--- src/main/kotlin/com/improve_future/harmonica/plugin/MigrationCreate.kt
+++ src/main/kotlin/com/improve_future/harmonica/plugin/MigrationCreate.kt
@@ -2,10 +2,35 @@ package com.improve_future.harmonica.plugin
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.*
 
-open class MigrationCreate: DefaultTask() {
+open class MigrationCreate: AbstractTask() {
+    private val dateFormat: SimpleDateFormat =
+            SimpleDateFormat("yyyyMMddHHmmssSSS")
+
     @TaskAction
     fun createMigration() {
+        val version = dateFormat.format(Date())
+        val migrationFile = Paths.get(
+                findMigrationDir().absolutePath, "${version}_XXXXXXXX.kts").toFile()
+        migrationFile.parentFile.mkdirs()
+        migrationFile.createNewFile()
+        migrationFile.writeText("""import com.improve_future.harmonica.core.AbstractMigration
 
+object : AbstractMigration() {
+    override fun up() {
+        createTable("table_name') {
+            integer("column_1")
+            varchar("column_2")
+        }
+    }
+
+    override fun down() {
+        dropTable("table_name")
+    }
+}""")
+        println("Created ${migrationFile.absolutePath}")
     }
 }

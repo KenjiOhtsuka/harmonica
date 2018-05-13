9945904db2a3e61b29701e8040aea1edc42eb3c5
Kenji Otsuka <kenji.otsuka@eys-style.com>
2018-05-13 21:27:41 +0900

    update column class

--- src/main/kotlin/com/improve_future/harmonica/core/Column.kt
+++ src/main/kotlin/com/improve_future/harmonica/core/Column.kt
@@ -6,11 +6,23 @@ class Column(val name: String, val type: Type) {
     val sqlType: String
         get() {
             return when (type) {
-                Types.VARCHAR -> "VARCHAR"
-                Types.INTEGER -> "INTEGER"
-                Types.BOOLEAN -> "BOOL"
+                Types.BIGINT -> "BIGINT" // BITSERIAL
+                Types.BIT -> "BIT"
+                Types.BINARY -> "BIT VARYING"
                 Types.BLOB -> "BLOB"
+                Types.BOOLEAN -> "BOOL"
+                Types.LONGVARBINARY -> "BYTEA"
+                Types.CHAR -> "CHARACTER"
                 Types.DATE -> "DATE"
+                Types.DOUBLE -> "DOUBLE PRECISION"
+                Types.NUMERIC -> "NUMERIC"
+                Types.REAL -> "REAL"
+                Types.SMALLINT -> "SMALLINT"
+                Types.LONGNVARCHAR -> "TEXT"
+                Types.TIMESTAMP -> "TIMESTAMP" // TIME WITH TIMEZONE, TIMESTAMP WITH TIMEZONE
+                Types.VARCHAR -> "VARCHAR"
+                Types.INTEGER -> "INTEGER" // SERIAL
+                Types.SQLXML -> "XML"
                 else -> throw Exception()
             }
         }

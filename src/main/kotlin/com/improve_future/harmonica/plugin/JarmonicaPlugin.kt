4956c664d657dfa7a4bdb435db6a0815ca4abe08
Kenji Otsuka <kok.fdcm@gmail.com>
2018-05-22 10:53:27 +0900

    #8 add two tasks

--- src/main/kotlin/com/improve_future/harmonica/plugin/JarmonicaPlugin.kt
+++ src/main/kotlin/com/improve_future/harmonica/plugin/JarmonicaPlugin.kt
@@ -1,22 +1,13 @@
 package com.improve_future.harmonica.plugin
 
-import com.improve_future.harmonica.core.DbConfig
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.internal.classloader.ClasspathUtil.getClasspath
-import com.sun.javafx.scene.CameraHelper.project
 import groovy.lang.Closure
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.plugins.ApplicationPlugin
 import org.gradle.api.plugins.JavaPluginConvention
-import org.codehaus.groovy.vmplugin.VMPluginFactory.getPlugin
 import org.gradle.api.tasks.JavaExec
-import com.sun.javafx.scene.CameraHelper.project
-import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
-import java.io.File
-import java.nio.file.Paths
-import javax.script.ScriptEngineManager
 
 fun <T> T.groovyClosure(function: () -> Unit) = object : Closure<Unit>(this) {
     @Suppress("unused")
@@ -41,12 +32,13 @@ class JarmonicaPluginAction : Action<Project> {
     override fun execute(project: Project) {
         val javaConvention = project.convention
                 .getPlugin(JavaPluginConvention::class.java)
-        val task: JarmonicaUpTask = project.tasks.create("jarmonicaUp", JarmonicaUpTask::class.java)
-            task.description = "Runs this project as a Spring Boot application."
-            task.group = ApplicationPlugin.APPLICATION_GROUP
-            task.classpath(javaConvention.sourceSets
+        val task1: JarmonicaUpTask = project.tasks.create("jarmonicaUp", JarmonicaUpTask::class.java)
+        task1.run {
+            description = "Compile and migrate up."
+            group = ApplicationPlugin.APPLICATION_GROUP
+            classpath(javaConvention.sourceSets
                     .findByName(SourceSet.MAIN_SOURCE_SET_NAME)!!.runtimeClasspath)
-            task.conventionMapping.map(
+            conventionMapping.map(
                     "jvmArgs",
                     groovyClosure {
                         if (project.hasProperty("applicationDefaultJvmArgs"))
@@ -54,22 +46,42 @@ class JarmonicaPluginAction : Action<Project> {
                         else java.util.Collections.emptyList<Any>()
                     }
             )
-            task.conventionMapping("main", { "com.improve_future.harmonica.Main" })
+            conventionMapping("main", { "com.improve_future.harmonica.Main" })
+            //conventionMapping("main",
+            //        MainClassConvention(project, ???({ run.getClasspath() })))
+        }
+        val task2: JarmonicaDownTask = project.tasks.create("jarmonicaDown", JarmonicaDownTask::class.java)
+        task2.run {
+            description = "Compile and migrate down."
+            // ToDo: change to Harmonica
+            group = ApplicationPlugin.APPLICATION_GROUP
+            classpath(javaConvention.sourceSets
+                    .findByName(SourceSet.MAIN_SOURCE_SET_NAME)!!.runtimeClasspath)
+            conventionMapping.map(
+                    "jvmArgs",
+                    groovyClosure {
+                        if (project.hasProperty("applicationDefaultJvmArgs"))
+                            project.property("applicationDefaultJvmArgs")
+                        else java.util.Collections.emptyList<Any>()
+                    }
+            )
+            conventionMapping("main", { "com.improve_future.harmonica.Main" })
             //conventionMapping("main",
             //        MainClassConvention(project, ???({ run.getClasspath() })))
         }
     }
+}
 
-
-
-open class JarmonicaUpTask : JavaExec() {
-    private val migrationPackage: String
+abstract class JarmonicaMigrationTask : JavaExec() {
+    protected val migrationPackage: String
         get() {
             if (project.extensions.extraProperties.has("migrationPackage"))
                 return project.extensions.extraProperties["migrationPackage"] as String
             return "db"
         }
+}
 
+open class JarmonicaUpTask : JarmonicaMigrationTask() {
 //    private val env: String
 //        get() {
 //            if (project.extensions.extraProperties.has("env"))
@@ -99,8 +111,25 @@ open class JarmonicaUpTask : JavaExec() {
 
     override fun exec() {
         jvmArgs = listOf<String>()//migrationPackage)
-        args = listOf(migrationPackage)
-        println("-- print main --")
+        args = listOf(migrationPackage, JarmonicaTaskType.Up.toString())
+        println(main)
+        super.exec()
+    }
+}
+
+open class JarmonicaDownTask : JarmonicaMigrationTask() {
+    override fun exec() {
+        jvmArgs = listOf<String>()//migrationPackage)
+        args = listOf(migrationPackage, JarmonicaTaskType.Down.toString())
+        println(main)
+        super.exec()
+    }
+}
+
+open class JarmonicaCreateTask : JarmonicaMigrationTask() {
+    override fun exec() {
+        jvmArgs = listOf<String>()//migrationPackage)
+        args = listOf(migrationPackage, JarmonicaTaskType.Create.toString())
         println(main)
         super.exec()
     }
@@ -112,4 +141,10 @@ class JarmonicaArgument() {
     fun toArray() {
         arrayOf(migrationPackage)
     }
+}
+
+enum class JarmonicaTaskType {
+    Up,
+    Down,
+    Create
 }

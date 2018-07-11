5df728d6cbcd58a39e39326f1a873478e5955702
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-11 12:37:23 +0900

    split files

---
+++ src/main/kotlin/com/improve_future/harmonica/plugin/JarmonicaCreateTask.kt
@@ -0,0 +1,16 @@
+package com.improve_future.harmonica.plugin
+
+open class JarmonicaCreateTask : JarmonicaMigrationTask() {
+    override val taskType = JarmonicaTaskType.Create
+
+    override fun exec() {
+        val migrationName =
+                if (project.hasProperty("migrationName"))
+                    project.properties["migrationName"] as String
+                else "Migration"
+
+        jvmArgs = listOf<String>()
+        args = buildJarmonicaArgument(migrationName).toList()
+        super.exec()
+    }
+}
--- src/main/kotlin/com/improve_future/harmonica/plugin/JarmonicaPlugin.kt
+++ src/main/kotlin/com/improve_future/harmonica/plugin/JarmonicaPlugin.kt
@@ -80,18 +80,3 @@ open class JarmonicaDownTask : JarmonicaMigrationTask() {
         super.exec()
     }
 }
-
-open class JarmonicaCreateTask : JarmonicaMigrationTask() {
-    override val taskType = JarmonicaTaskType.Create
-
-    override fun exec() {
-        val migrationName =
-                if (project.hasProperty("migrationName"))
-                    project.properties["migrationName"] as String
-                else "Migration"
-
-        jvmArgs = listOf<String>()
-        args = buildJarmonicaArgument(migrationName).toList()
-        super.exec()
-    }
-}

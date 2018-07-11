d7f481624b8827d91e95aba81db648c5fbb9e919
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-11 22:36:41 +0900

    update document

--- document/src/main/kotlin/com/improve_future/harmonica/document/view/JarmonicaView.kt
+++ document/src/main/kotlin/com/improve_future/harmonica/document/view/JarmonicaView.kt
@@ -1,11 +1,25 @@
 package com.improve_future.harmonica.document.view
 
+import com.improve_future.harmonica.document.helper.row
+import kotlinx.html.div
+import kotlinx.html.h1
+import kotlinx.html.p
+
 object JarmonicaView : ViewInterface {
     override val path = "jarmonica.html"
 
     override fun index(): String {
         return Template.default("Home") {
-
+            row {
+                div("col") {
+                    h1 { +"Jarmonica" }
+                }
+            }
+            row {
+                div("col") {
+                    p { +"Jarmonica, migration with compiled Kotlin classes." }
+                }
+            }
         }
     }
 }

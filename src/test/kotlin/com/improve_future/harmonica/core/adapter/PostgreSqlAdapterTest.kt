8e607a4b026607c6ed885bfc19257118116c560f
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-12 18:22:41 +0900

    update test

--- src/test/kotlin/com/improve_future/harmonica/core/adapter/PostgreSqlAdapterTest.kt
+++ src/test/kotlin/com/improve_future/harmonica/core/adapter/PostgreSqlAdapterTest.kt
@@ -2,27 +2,62 @@ package com.improve_future.harmonica.core.adapter
 
 import com.improve_future.harmonica.core.table.column.AbstractColumn
 import com.improve_future.harmonica.core.table.column.IntegerColumn
+import com.improve_future.harmonica.core.table.column.VarcharColumn
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.asserter
 
 class PostgreSqlAdapterTest {
+    val buildColumnDeclarationFunctionForTest =
+            PostgreSqlAdapter.Companion::class.java.getDeclaredMethod(
+                    "buildColumnDeclarationForCreateTableSql",
+                    AbstractColumn::class.java).also { it.isAccessible = true }
+
     @Test
-    fun testBuildColumnDeclarationForCreateTableSql() {
-        val buildColumnDeclarationForCreateTableSql =
-                PostgreSqlAdapter.Companion::class.java.getDeclaredMethod(
-                        "buildColumnDeclarationForCreateTableSql",
-                        AbstractColumn::class.java)
-        buildColumnDeclarationForCreateTableSql.isAccessible = true
-
+    fun testBuildColumnDeclarationForInteger() {
         val integerColumn = IntegerColumn("int")
-        integerColumn.nullable = false
 
-        val expression =
-                buildColumnDeclarationForCreateTableSql.invoke(
+        fun buildIntegerDeclaration() =
+                buildColumnDeclarationFunctionForTest.invoke(
                         PostgreSqlAdapter, integerColumn)
 
         assertEquals(
+                "int INTEGER",
+                buildIntegerDeclaration())
+        integerColumn.nullable = false
+        assertEquals(
                 "int INTEGER NOT NULL",
-                expression)
+                buildIntegerDeclaration())
+        integerColumn.default = 1
+        assertEquals(
+                "int INTEGER NOT NULL DEFAULT 1",
+                buildIntegerDeclaration())
+        integerColumn.nullable = true
+        assertEquals(
+                "int INTEGER DEFAULT 1",
+                buildIntegerDeclaration())
+    }
+
+    @Test
+    fun testBuildColumnDeclarationForVarchar() {
+        val varcharColumn = VarcharColumn("varchar")
+        fun buildVarcharDeclaration() =
+                buildColumnDeclarationFunctionForTest.invoke(
+                        PostgreSqlAdapter, varcharColumn)
+        assertEquals(
+                "varchar VARCHAR",
+                buildVarcharDeclaration())
+        varcharColumn.nullable = false
+        assertEquals(
+                "varchar VARCHAR NOT NULL",
+                buildVarcharDeclaration())
+        varcharColumn.default = "varchar"
+        assertEquals(
+                "varchar VARCHAR NOT NULL DEFAULT 'varchar'",
+                buildVarcharDeclaration())
+        varcharColumn.nullable = true
+        assertEquals(
+                "varchar VARCHAR DEFAULT 'varchar'",
+                buildVarcharDeclaration())
     }
 }

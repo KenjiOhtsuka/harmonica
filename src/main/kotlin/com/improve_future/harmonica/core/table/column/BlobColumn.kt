7ff293fb1e3983beedfebeb134dec9d2cdb38461
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-18 12:37:54 +0900

    #36 #37 add BlogColumn

---
+++ src/main/kotlin/com/improve_future/harmonica/core/table/column/BlobColumn.kt
@@ -0,0 +1,7 @@
+package com.improve_future.harmonica.core.table.column
+
+class BlobColumn(name: String) : AbstractColumn(name, java.sql.Types.BLOB) {
+    val default: ByteArray? = null
+    override val sqlDefault = "" // ToDo
+    override val hasDefault = default != null
+}

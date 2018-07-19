8bdc92b1e409e76a26a976d7681a77620daca668
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-19 22:49:25 +0900

    #46 add datetime column

--- src/test/kotlin/com/improve_future/harmonica/core/table/column/DateTimeColumnTest.kt
+++ src/test/kotlin/com/improve_future/harmonica/core/table/column/DateTimeColumnTest.kt
@@ -11,5 +11,12 @@ class DateTimeColumnTest {
         assertEquals(false, dateTimeColumn.hasDefault)
         dateTimeColumn.default = "2018-01-02 21:24:23.234"
         assertEquals(true, dateTimeColumn.hasDefault)
+        assertEquals(2018, dateTimeColumn.defaultLocalDateTime?.year)
+        assertEquals(1, dateTimeColumn.defaultLocalDateTime?.monthValue)
+        assertEquals(2, dateTimeColumn.defaultLocalDateTime?.dayOfMonth)
+        assertEquals(21, dateTimeColumn.defaultLocalDateTime?.hour)
+        assertEquals(24, dateTimeColumn.defaultLocalDateTime?.minute)
+        assertEquals(23, dateTimeColumn.defaultLocalDateTime?.second)
+        assertEquals(234000000, dateTimeColumn.defaultLocalDateTime?.nano)
     }
 }

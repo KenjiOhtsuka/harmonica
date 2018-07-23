c7d80476455c6483e32303400adf24540c3135fe
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-24 08:01:53 +0900

    update StubDbAdapter

--- src/test/kotlin/com/improve_future/harmonica/core/adapter/StubDbAdapter.kt
+++ src/test/kotlin/com/improve_future/harmonica/core/adapter/StubDbAdapter.kt
@@ -1,14 +1,15 @@
 package com.improve_future.harmonica.core.adapter
 
+import com.improve_future.harmonica.core.table.IndexMethod
 import com.improve_future.harmonica.core.table.TableBuilder
 import com.improve_future.harmonica.core.table.column.AbstractColumn
 import com.improve_future.harmonica.core.table.column.AddingColumnOption
 import com.improve_future.harmonica.stub.core.StubConnection
 
 internal class StubDbAdapter : DbAdapter(StubConnection()) {
-    internal val addingColumnList = mutableListOf<AddingColumn>()
+    val addingColumnList = mutableListOf<AddingColumn>()
 
-    internal data class AddingColumn(
+    data class AddingColumn(
         val tableName: String,
         val column: AbstractColumn,
         val option: AddingColumnOption
@@ -20,10 +21,13 @@ internal class StubDbAdapter : DbAdapter(StubConnection()) {
     override fun renameTable(oldTableName: String, newTableName: String) {
     }
 
-    override fun renameColumn(tableName: String, oldColumnName: String, newColumnName: String) {
+    override fun createIndex(
+        tableName: String, columnName: String, unique: Boolean,
+        method: IndexMethod?
+    ) {
     }
 
-    override fun createIndex(tableName: String, columnName: String, unique: Boolean) {
+    override fun renameIndex(tableName: String, oldIndexName: String, newIndexName: String) {
     }
 
     override fun dropIndex(tableName: String, indexName: String) {
@@ -32,5 +36,4 @@ internal class StubDbAdapter : DbAdapter(StubConnection()) {
     override fun addColumn(tableName: String, column: AbstractColumn, option: AddingColumnOption) {
         addingColumnList.add(AddingColumn(tableName, column, option))
     }
-
 }

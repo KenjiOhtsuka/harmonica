1645a0af6e21a519dafaf7a068354c8d1789d5aa
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-08-29 08:12:13 +0900

    update doc

--- document/src/main/kotlin/com/improve_future/harmonica/document/view/Template.kt
+++ document/src/main/kotlin/com/improve_future/harmonica/document/view/Template.kt
@@ -11,6 +11,8 @@ object Template {
             appendln("<!DOCTYPE html>")
             appendHTML().html {
                 head {
+                    meta(name="charset", content="UTF-8")
+                    meta(name="viewport", content="width=devise-width, initial-scale=1.0")
                     title(title)
                     link(
                         "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css",

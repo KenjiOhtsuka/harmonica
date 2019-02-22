837d90434d6cf26271e0d5c67430a01f6006662d
Kenji Ohtsuka <kok.fdcm@gmail.com>
2019-02-22 23:21:25 +0900

    add copyright

--- src/main/kotlin/com/improve_future/harmonica/config/PluginConfig.kt
+++ src/main/kotlin/com/improve_future/harmonica/config/PluginConfig.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Harmonica.
+ *
+ * Harmonica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.improve_future.harmonica.config
 
 internal object PluginConfig {
@@ -10,4 +27,10 @@ internal object PluginConfig {
             false
         }
     }
+
+//    object Exposed {
+//        fun getTransactionManager(): Class<*> {
+//            return Class.forName("org.jetbrains.exposed.sql.transactions.TransactionManager")
+//        }
+//    }
 }

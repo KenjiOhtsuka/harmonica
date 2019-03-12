cbee65cb441683aea98ea7e8647e537f960ea3d3
Kenji Ohtsuka <kok.fdcm@gmail.com>
2019-03-12 21:01:54 +0900

    refactor

--- src/test/kotlin/com/improve_future/harmonica/plugin/JarmonicaArgumentTest.kt
+++ src/test/kotlin/com/improve_future/harmonica/plugin/JarmonicaArgumentTest.kt
@@ -23,24 +23,6 @@ import kotlin.test.assertEquals
 
 class JarmonicaArgumentTest {
     @Test
-    fun testToList() {
-        val arg = JarmonicaArgument()
-        arg.migrationDirectory = "directory"
-        arg.taskType = JarmonicaTaskType.Up
-        arg.env = "env"
-        arg.migrationPackage = "package"
-
-        var expectedList = mutableListOf(
-            "package", "Up", "directory", "env", "false"
-        )
-        assertEquals(expectedList, arg.toList())
-
-        arg.add("")
-        expectedList.add("")
-        assertEquals(expectedList, arg.toList())
-    }
-
-    @Test
     fun testToArray() {
         val arg = JarmonicaArgument()
         arg.migrationDirectory = "directory"

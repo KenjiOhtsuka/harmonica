d0ade6271917c34cca85779610f22c76933193bd
Kenji Ohtsuka <kok.fdcm@gmail.com>
2019-04-27 19:30:47 +0900

    update

--- src/main/kotlin/com/improve_future/harmonica/core/Connection.kt
+++ src/main/kotlin/com/improve_future/harmonica/core/Connection.kt
@@ -30,8 +30,6 @@ open class Connection(
     private lateinit var coreConnection: java.sql.Connection
 
     private fun connect(config: DbConfig) {
-        println(buildConnectionUriFromDbConfig(config))
-
         if (config.dbms != Dbms.SQLite) {
             coreConnection =
                 object : java.sql.Connection by DriverManager.getConnection(

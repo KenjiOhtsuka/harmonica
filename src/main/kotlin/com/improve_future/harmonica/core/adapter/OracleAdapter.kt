ac307bd8887b9cc9e7acd0e49890afa3928e243d
Kenji Ohtsuka <kok.fdcm@gmail.com>
2019-08-04 21:44:53 +0900

    (feat) oracle create table

--- src/main/kotlin/com/improve_future/harmonica/core/adapter/OracleAdapter.kt
+++ src/main/kotlin/com/improve_future/harmonica/core/adapter/OracleAdapter.kt
@@ -21,12 +21,88 @@ package com.improve_future.harmonica.core.adapter
 import com.improve_future.harmonica.core.ConnectionInterface
 import com.improve_future.harmonica.core.table.IndexMethod
 import com.improve_future.harmonica.core.table.TableBuilder
-import com.improve_future.harmonica.core.table.column.AbstractColumn
-import com.improve_future.harmonica.core.table.column.AddingColumnOption
+import com.improve_future.harmonica.core.table.column.*
 
 internal class OracleAdapter(connection: ConnectionInterface) : DbAdapter(connection) {
     override fun createTable(tableName: String, tableBuilder: TableBuilder) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var sql = "CREATE TABLE $tableName (\n"
+        if (tableBuilder.id) {
+            sql += "  id NUMBER GENERATED ALWAYS AS IDENTITY"
+            if (tableBuilder.columnList.size > 0) sql += ','
+            sql += "\n"
+        }
+        sql += tableBuilder.columnList.joinToString(",\n") {
+            "  " + buildColumnDeclarationForCreateTableSql(it)
+        }
+        sql += "\n);"
+        execute(sql)
+
+        // Add Comments
+        if (tableBuilder.comment != null) {
+            sql = "COMMENT ON TABLE $tableName IS '${tableBuilder.comment}';"
+            execute(sql)
+        }
+        tableBuilder.columnList.forEach {
+            if (it.hasComment) {
+                sql = "COMMENT ON COLUMN $tableName.${it.name} IS" +
+                        " '${it.comment}';"
+                execute(sql)
+            }
+        }
+    }
+
+    internal companion object : DbAdapter.CompanionInterface() {
+        private fun buildColumnDeclarationForCreateTableSql(
+            column: AbstractColumn
+        ): String {
+            var sql = column.name + " " + sqlType(column)
+            when (column) {
+                is VarcharColumn -> {
+                    sql +=
+                        if (column.size == null) "(255)"
+                        else "(" + column.size + ")"
+                }
+                is DecimalColumn -> {
+                    if (column.precision != null) {
+                        sql += "(" + column.precision.toString()
+                        if (column.scale != null) {
+                            sql += ", " + column.scale.toString()
+                        }
+                        sql += ")"
+                    }
+                }
+//                is TimeZoneInterface -> {
+//                    if (column.withTimeZone) {
+//                        sql += " WITH TIME ZONE"
+//                    }
+//                }
+            }
+            if (!column.nullable) sql += " NOT NULL"
+            if (column.hasDefault) {
+                sql += " DEFAULT " + column.sqlDefault
+            }
+//            if (column.hasReference)
+//                sql += " REFERENCES ${column.referenceTable} (${column.referenceColumn})"
+            return sql
+        }
+
+        override fun sqlType(column: AbstractColumn): String {
+            return when (column) {
+                else -> super.sqlType(column)
+            }
+        }
+
+        override fun sqlIndexMethod(method: IndexMethod?): String? {
+            return when (method) {
+//                IndexMethod.BTree -> "btree"
+//                IndexMethod.Hash -> "hash"
+//                IndexMethod.Gist -> "gist"
+//                IndexMethod.SpGist -> "spgist"
+//                IndexMethod.Gin -> "gin"
+//                IndexMethod.BRin -> "brin"
+                else -> null
+            }
+        }
     }
 
     override fun createIndex(
@@ -35,7 +111,14 @@ internal class OracleAdapter(connection: ConnectionInterface) : DbAdapter(connec
         unique: Boolean,
         method: IndexMethod?
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var sql = "CREATE"
+        if (unique) sql += " UNIQUE"
+        // for bitmap index: if (bitmap) sql += " BITMAP"
+        sql += " INDEX ${tableName}_${columnNameArray.joinToString("_")}_idx" +
+                " ON $tableName"
+        //PostgreSqlAdapter.sqlIndexMethod(method)?.let { sql += " USING $it" }
+        sql += " (${columnNameArray.joinToString(", ")});"
+        execute(sql)
     }
 
     override fun dropIndex(tableName: String, indexName: String) {

61166f1a5958ad1e9246daafd4dd99e2424a47fa
Kenji Ohtsuka <kok.fdcm@gmail.com>
2020-06-15 07:27:37 +0900

    update jdk

--- src/test/kotlin/com/improve_future/harmonica/core/StubMigration.kt
+++ src/test/kotlin/com/improve_future/harmonica/core/StubMigration.kt
@@ -27,17 +27,14 @@ internal class StubMigration : AbstractMigration() {
         .getDeclaredField("adapter\$delegate").also {
             it.isAccessible = true
         }
+
     val adapter = StubDbAdapter()
 
     init {
         connection = StubConnection()
         delegateAdapterField.also { field ->
             field.isAccessible = true
-            val modifier = field::class.java.getDeclaredField("modifiers")
-            modifier.isAccessible = true
-            modifier.setInt(field, field.modifiers and Modifier.FINAL.inv())
             field.set(this as AbstractMigration, lazyOf(adapter))
         }
     }
-
 }
